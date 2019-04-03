44adc7be453ee53345bdddcf8a7e411ac4a00e7b
rexsidirtjump <a9rexsi22@gmail.com>
2019-04-03 23:35:38 +0700

    Belajar Python Dasar List

---
+++ 3-list.py
@@ -0,0 +1,28 @@
+Data = [1,2,3,4,5,6,7,8,9]
+Data2 = [100,200,300,400,500,600,700,800,900]
+
+
+#mengakses List
+Subdata1 = Data[3]
+Subdata2 = Data[-3]
+
+#memotong List
+Subdata3 = Data[0:5]
+Subdata4 = Data[2:]
+
+#menambah List
+
+Data3 = Data + Data2
+
+#merubah contet dari list
+
+print(Data)
+Data[4] = 875
+
+#merubah contet dari list
+
+a = Data
+a[4] = 75
+
+print(a)
+
