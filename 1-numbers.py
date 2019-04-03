989b9b4b2ec8062051c8d2ff4ba18cdca0aedd60
rexsidirtjump <a9rexsi22@gmail.com>
2019-04-03 23:02:19 +0700

    Belajar Python Dasar Number

---
+++ 1-numbers.py
@@ -0,0 +1,24 @@
+a = 5
+b = 17
+c = 20
+
+#Penjumlahan
+
+d = a + b
+print(d)
+
+#Perkalian
+
+d = a * b
+print(d)
+
+#Pengurangan
+
+d = a - b
+print(d)
+
+#Pembagian | otomatis menjadi float
+
+d = a / b
+print(d)
+
