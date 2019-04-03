ff23c45ce852a55a7cac5ec12123d7244a150e02
rexsidirtjump <a9rexsi22@gmail.com>
2019-04-03 23:08:11 +0700

    Belajar Python Dasar String

---
+++ 2-strings.py
@@ -0,0 +1,27 @@
+text1 = 'ini adalah data string'
+
+text2 = 'ini adalah hari jum\'at'
+
+text3 = "jalan - jalan di hari jum'at"
+
+text4 = 'Rexsi Berkata "Kemarin Kemana sayang ?"'
+
+text5 = "Lita Berkata \" Jalan-Jalan Sayang\""
+
+#Give Enter / Break
+text6 = 'Rexsi Berkata "Kemarin Kemana sayang ?" \n Lita Berkata " Jalan-Jalan Sayang"'
+
+#Multi Line
+text7 = """
+Rexsi : "Hallo..."
+Lita : "Hai"
+Rexsi : "Lagi apa sayang ?"
+Lita : "Lagi kangen kamu :-p"
+"""
+
+#raw string   (r)
+text8 = r'C:\nyoto'
+
+
+print(text8)
+
